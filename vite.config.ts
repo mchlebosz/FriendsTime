bedc82215e52117a9258d4cd90eb5d538fd1c313
Mateusz Chlebosz <mateusz.chlebosz@outlook.com>
2025-04-07 18:52:39 +0200

    Icon changes

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       },
       includeAssets: ['favicon.ico', 'favicon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
+        id: '/FriendsTime/',
         name: 'FriendsTime',
         short_name: 'FriendsTime',
         description: 'A world clock app for your friends',
@@ -26,18 +27,31 @@ export default defineConfig({
         scope: '/FriendsTime/', // Important for GitHub Pages
         background_color: '#ffffff',
         theme_color: '#3eaf7c',
+        lang: 'en-US',
+        orientation: 'any',
+        iarc_rating_id: 'E',
+        categories: ['productivity', 'social'],
         icons: [
           {
-            src: '/FriendsTime/pwa-192x192.png',
-            sizes: '192x192',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
             type: 'image/png',
-            purpose: 'any maskable',
           },
           {
-            src: '/FriendsTime/pwa-512x512.png',
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
         screenshots: [
