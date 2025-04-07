6ecf948a9df81a21b7e36586245e11df20169fb4
Mateusz Chlebosz <mateusz.chlebosz@outlook.com>
2025-04-07 18:05:17 +0200

    Minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -28,25 +28,25 @@ export default defineConfig({
         theme_color: '#3eaf7c',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/FriendsTime/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/FriendsTime/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
         ],
         screenshots: [
           {
-            src: 'screenshot-mobile.png',
+            src: '/FriendsTime/screenshot-mobile.png',
             sizes: '360x640',
             type: 'image/png',
             form_factor: 'narrow',
           },
           {
-            src: 'screenshot-desktop.png',
+            src: '/FriendsTime/screenshot-desktop.png',
             sizes: '1280x800',
             type: 'image/png',
             form_factor: 'wide',
