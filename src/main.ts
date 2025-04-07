d78bd096ff77e60b4e6c7e2cba3a600d7eb3c0bd
Mateusz Chlebosz <mateusz.chlebosz@outlook.com>
2025-04-07 18:14:51 +0200

    Debugging

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,16 @@ const isDark = ref(false)
 app.provide('isDark', isDark)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/FriendsTime/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope)
+      })
+      .catch((error) => {
+        console.error('ServiceWorker registration failed: ', error)
+      })
+  })
+}
